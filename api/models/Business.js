caffe16213c0c04fa113e6a63c76ffbf6453a52e
Alexis <ali.finney@gmail.com>
2018-01-15 15:26:34 -0700

    learning basic sails

---
+++ api/models/Business.js
@@ -0,0 +1,21 @@
+/**
+ * Business.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    companyName :{ 
+      required: true,
+      type: 'string'
+    },
+    email : {
+      required: true,
+      type: 'string'
+    }
+  }
+};
+
